50161b041f2b426b9a27ddcdb78a269e5770977e
lyy <756669461@qq.com>
2017-04-11 10:16:34 +0800

    'helloword'

---
+++ Helloword1/app/src/main/java/com/example/administrator/helloword1/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.administrator.helloword1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button bt= (Button) findViewById(R.id.button);
+    }
+}
